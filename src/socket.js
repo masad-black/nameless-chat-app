947190b6ef67623759bdca3a2eb808b337f743bf
masad@black <asadbutt.dev@gmail.com>
2026-02-03 22:46:22 +0500

    feat: added typing loader

--- src/socket.js
+++ src/socket.js
@@ -9,23 +9,18 @@ import {
   NEW_MESSAGE_EVENT,
   RECEIVED_CONVERSATION_EVENT,
   RECEIVED_MESSAGE_EVENT,
+  RECEIVED_STOP_TYPING_EVENT,
+  SHOW_TYPING_EVENT,
+  STOP_TYPING_EVENT,
+  TYPING_EVENT,
 } from "./constant.js";
 
 // this will simpley add the socket to the room
-function joinRoom(socket, io) {
+function joinRoom(socket) {
   socket.on(JOIN_ROOM_EVENT, (conversationId) => {
     console.log(`User joined the room conversationId: `, conversationId);
 
     socket.join(`${conversationId}`);
-    console.log("Total Rooms On the Server: ", io.sockets.adapter.rooms);
-  });
-}
-
-function sendMessage(socket) {
-  socket.on(NEW_MESSAGE_EVENT, (conversationId, message) => {
-    console.log(`New Message from user: ${message} and id: ${conversationId}`);
-
-    socket.to(`${conversationId}`).emit(NEW_MESSAGE_EVENT, conversationId, message);
   });
 }
 
@@ -51,6 +46,18 @@ function listeningAndSendingMessag(socket) {
   });
 }
 
+function listeningTyping(socket) {
+  socket.on(TYPING_EVENT, ({ conversationId }) => {
+    socket.to(conversationId).emit(SHOW_TYPING_EVENT, { response: true });
+  });
+}
+
+function listeningToStopTyping(socket) {
+  socket.on(STOP_TYPING_EVENT, (conversationId) => {
+    socket.to(conversationId).emit(RECEIVED_STOP_TYPING_EVENT);
+  });
+}
+
 export default function runSocketServer(io) {
   try {
     io.on(CONNECTION_EVENT, (socket) => {
@@ -64,8 +71,11 @@ export default function runSocketServer(io) {
       socket.emit(CLIENT_CONNECTED_EVENT, { response: true });
       console.log(`Client connected to the server and user id: ${socket.user.id}`);
 
+      joinRoom(socket);
       listeningAndSendConversation(socket);
       listeningAndSendingMessag(socket);
+      listeningTyping(socket);
+      listeningToStopTyping(socket);
 
       // when client disconnect from the server
       socket.on(DISCONNECT_EVENT, () => {
